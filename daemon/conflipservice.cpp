e31e5bb397f84b22b4bc51c95b9af320792ba092
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-08-11 17:26:59 +0200

    added cli for logging control

--- daemon/conflipservice.cpp
+++ daemon/conflipservice.cpp
@@ -1,4 +1,6 @@
 #include "conflipservice.h"
+#include <QCommandLineParser>
+#include <QLoggingCategory>
 
 ConflipService::ConflipService(int &argc, char **argv) :
 	Service(argc, argv)
@@ -16,6 +18,41 @@ void ConflipService::setSlice(const QString &slice)
 
 QtService::Service::CommandMode ConflipService::onStart()
 {
+	QCommandLineParser parser;
+	parser.addOption({QStringLiteral("backend"), {}});
+	parser.addOption({QStringLiteral("slice"), {}});
+	parser.addOption({QStringLiteral("loglevel"), {}, QStringLiteral("level"),
+#ifdef QT_NO_DEBUG
+					  QString::number(3)});
+#else
+					  QString::number(4)});
+#endif
+	if(!parser.parse(QCoreApplication::arguments())) {
+		qCritical().noquote() << parser.errorText();
+		qApp->exit(EXIT_FAILURE);
+		return Synchronous;
+	}
+
+	QString logStr;
+	switch (parser.value(QStringLiteral("loglevel")).toInt()) {
+	case 0:
+		logStr.prepend(QStringLiteral("\n*.critical=false"));
+		Q_FALLTHROUGH();
+	case 1:
+		logStr.prepend(QStringLiteral("\n*.warning=false"));
+		Q_FALLTHROUGH();
+	case 2:
+		logStr.prepend(QStringLiteral("\n*.info=false"));
+		Q_FALLTHROUGH();
+	case 3:
+		logStr.prepend(QStringLiteral("*.debug=false"));
+		Q_FALLTHROUGH();
+	case 4:
+	default:
+		break;
+	}
+	QLoggingCategory::setFilterRules(logStr);
+
 	_engine = new SyncEngine{this};
 	if(!_engine->start())
 		qApp->exit(EXIT_FAILURE);
